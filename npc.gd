6eb934cff060413645bbcdadf338c8ed66fdd8ac
Kavusiaaa <krzysgruszczyk11@gmail.com>
2026-06-09 16:00:44 +0200

    chattingSystem1
    Początek skryptu z dymkami

--- npc.gd
+++ npc.gd
@@ -1,11 +1,30 @@
 extends Node2D
+const speed = 30
+var current_state = IDLE
 
+var dir = Vector2.RIGHT
+var start_pos
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var is_roaming = true
+var is_chatting = false
 
+var player
+var player_in_chat_zone = false
+enum {
+	IDLE,
+	NEW_DIR,
+	MOVE
+}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	randomize()
+	start_pos = position
+func _process(delta):
+	if current_state == 0 or current_state == 1:
+		pass
+		#$Stickman.play("idle") - animacja idle #
+	elif current_state == 2 and !is_chatting:
+		if dir.x == -1:
+			pass
+			#$Stickman.play("walk_w") - do dokonczenia system 
+	
